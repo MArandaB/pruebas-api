800b6063bbefd074757728aa9bc935cbfa9867c9
MArandaB <mario_aranda2012@hotmail.com>
2022-11-25 10:24:42 +0100

    commit

---
+++ api-arasaac.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export const pedirPictos = async (searchText) => {
+
+  /* solicitar datos a api.arasaac.org*/
+
+  //1. saber como usar axios
+  //2. url de la api
+
+  const endPoint = `https://api.arasaac.org/api/pictograms/es/search/${searchText}`
+
+
+  // use axios
+  try {
+    const pictos = await axios.get(endPoint)
+    console.log(pictos)
+  }catch (error) {
+    console.log(error)
+  }
+
+}
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,19 +1,6 @@
-//TAREAS
-/*
-1.HACER UNA COMPILACION Y VER QUE FUNCIONE
-2. PONER UNA CAJA DE TEXTO, PARA BÚSQUEDA DINÁMICA
-3. MOSTRAR LAS IMAGENES (CONSULTAR API ARASAAC)
-4. DAR SUGERENCIAS DE BÚSQUEDA (ENDPOINT KEYWORDS EN API ARASAAC)
-
-
-
-*/
-
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
-import { cargarMapa } from './maps'
-import { pedirPictos } from './api-arasaac.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -34,5 +21,3 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
-cargarMapa()
-pedirPictos('barco')
